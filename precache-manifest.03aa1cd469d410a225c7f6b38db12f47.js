d9d3e9e474ff5c113efe5ede86b76a6af1286106
rajeevsinghh <123074976+rajeevsinghh@users.noreply.github.com>
2024-04-02 10:37:16 +0530

    Updates

---
+++ precache-manifest.03aa1cd469d410a225c7f6b38db12f47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17baeb7586e5a14b38613734216af740",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "025ceae3059017cd4def",
+    "url": "/Portfolio/static/css/main.22718620.chunk.css"
+  },
+  {
+    "revision": "612e55e952f864a03181",
+    "url": "/Portfolio/static/js/2.f0bb771f.chunk.js"
+  },
+  {
+    "revision": "025ceae3059017cd4def",
+    "url": "/Portfolio/static/js/main.06f062a0.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "6150bed734eb529492ec5a1351d30f2c",
+    "url": "/Portfolio/static/media/HeroImage.6150bed7.jpg"
+  }
+]);
